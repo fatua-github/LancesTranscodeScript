0c306b45402baf27de2dfeb7f58f75c0485596e9
Jeff Mathers <jmathers@probus.ca>
2016-06-06 07:06:14 -0400

    Updated .gitignore to ignore more processing batch files

--- gox265.bat
+++ gox265.bat
@@ -1,1 +1,1 @@
-powershell .\downloadtranscoder.ps1 -path Z:\Downloads\couchpotato\totranscode -codec hevc
+powershell .\downloadtranscoder.ps1 -path Z:\Downloads\couchpotato\totranscode -codec hevc -crf
